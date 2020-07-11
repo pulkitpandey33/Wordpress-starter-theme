c170c789e08246e531438f4bbcae899e0f92d31c
Pulkit Pandey <pulkitpandey171@gmail.com>
2020-07-11 18:26:20 +0530

    Add a code for initializing widgets and linking style on the header

--- functions.php
+++ functions.php
@@ -64,3 +64,25 @@ function custom_setup() {
 }
 endif;
 add_action( 'after_setup_theme', 'custom_setup' );
+
+function custom_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Footer', 'custom' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in your footer.', 'custom' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+}
+add_action( 'widgets_init', 'custom_widgets_init' );
+
+function custom_script() {
+	wp_enqueue_style( 'custom-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'custom_script' );
